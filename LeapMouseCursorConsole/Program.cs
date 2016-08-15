08bb78b686abb9fe6e7b4392c6469c31835c1ce2
tichinose1-oa <toyoh.ichinose@unisys.co.jp>
2016-08-15 22:40:10 +0900

    カーソル移動はあっさり実装できた

--- LeapMouseCursorConsole/Program.cs
+++ LeapMouseCursorConsole/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Leap;
+using LeapMouseCursorConsole.Properties;
+using System;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices;
 
 namespace LeapMouseCursorConsole
 {
@@ -10,6 +12,27 @@ namespace LeapMouseCursorConsole
     {
         static void Main(string[] args)
         {
+            var controller = new Controller();
+
+            Observable.Interval(TimeSpan.FromSeconds(1.0 / Settings.Default.FPS))
+                .Select(_ => controller.Frame().Fingers
+                    .Where(f => f.Type == Finger.FingerType.TYPE_INDEX)
+                    .SingleOrDefault())
+                .Where(f => f != null)
+                .Select(f => f.TipPosition)
+                .Do(p => Debug.WriteLine("x: {0}, y: {1}, z: {2}", p.x, p.y, p.z))
+                .Subscribe(p =>
+                {
+                    var x = Settings.Default.Width / 2 + p.x * Settings.Default.ScaleX;
+                    var y = Settings.Default.Height - p.y * Settings.Default.ScaleY;
+
+                    SetCursorPos((int)x, (int)y);
+                });
+
+            Console.ReadKey();
         }
+
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
     }
 }
