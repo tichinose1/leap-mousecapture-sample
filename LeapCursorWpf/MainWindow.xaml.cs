4d21cd12e982fa4c36e8ccfa075b9acad5370bbd
tichinose1-oa <toyoh.ichinose@unisys.co.jp>
2016-08-16 18:59:00 +0900

    カーソル画像変更まで実装したが、WPFウインドウ上でしか効果がないようだ

--- LeapCursorWpf/MainWindow.xaml.cs
+++ LeapCursorWpf/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Leap;
+using LeapCursorWpf.Properties;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace LeapCursorWpf
 {
@@ -20,9 +14,85 @@ namespace LeapCursorWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        Controller controller = null;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+
+            // Controllerの初期化
+            controller = new Controller();
+            controller.SetPolicyFlags(Controller.PolicyFlag.POLICY_BACKGROUND_FRAMES);
+
+            // 生データ
+            var raw = Observable.Interval(TimeSpan.FromSeconds(1.0 / Settings.Default.FPS))
+                .Select(_ => controller.Frame().Fingers
+                    .Where(f => f.Type == Finger.FingerType.TYPE_INDEX)
+                    .FirstOrDefault())
+                .Where(f => f != null)
+                //.Do(f => Debug.WriteLine("x: {0}, y: {1}, z: {2}", f.TipPosition.x, f.TipPosition.y, f.TipPosition.z))
+                .Select(f => f.TipPosition);
+
+            // カーソル移動
+            // 座標変換後、指定の数値の倍数に丸める
+            raw
+                .Select(p => new
+                {
+                    X = Settings.Default.ScreenX / 2 + Settings.Default.Scale * p.x,
+                    Y = Settings.Default.ScreenY + Settings.Default.SupplementY - Settings.Default.Scale * p.y,
+                })
+                .Select(a => new { X = Round(a.X), Y = Round(a.Y) })
+                .DistinctUntilChanged()
+                .Subscribe(a =>
+                {
+                    PInvoke.PerformMoveCursor(a.X, a.Y);
+                });
+
+            // 長押し
+            // 一定期間内に指先が常に指定範囲内にいるかどうかを監視
+            // クリック後は手前に引く必要あり
+            raw
+                .Select(p => p.z < Settings.Default.RangeInZ)
+                //.Do(b => Debug.WriteLine("b: {0}", b))
+                .Buffer(TimeSpan.FromSeconds(Settings.Default.LongClickTime))
+                .Select(bs => bs.All(b => b))
+                .DistinctUntilChanged()
+                .Where(b => b)
+                .Subscribe(_ =>
+                {
+                    PInvoke.PerformClick();
+                });
+
+            // カーソル画像変更
+            raw
+                .Select(p => p.z < Settings.Default.RangeInZ)
+                .DistinctUntilChanged()
+                .Where(b => b)
+                .Subscribe(_ =>
+                {
+                    SetCursor();
+                });
+        }
+
+        void SetCursor()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                Mouse.OverrideCursor = Cursors.Wait;
+            });
+        }
+
+        static int Round(double originalValue)
+        {
+            var n = Settings.Default.RoundCoefficient;
+            return ((int)originalValue / n) * n;
         }
     }
 }
---
+++ LeapCursorWpf/PInvoke.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LeapCursorWpf
+{
+    public static class PInvoke
+    {
+        const int MOUSEEVENTF_LEFTDOWN = 0x2;
+        const int MOUSEEVENTF_LEFTUP = 0x4;
+
+        public static void PerformClick()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+
+        public static void PerformMoveCursor(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+
+        [DllImport("USER32.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("User32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+    }
+}
